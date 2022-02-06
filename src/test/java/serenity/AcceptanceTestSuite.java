8c025718b9e76503f10aa03b61107d444012622a
Omar <omar.bouayad26@gmail.com>
2022-02-06 21:29:51 +0100

    p

--- src/test/java/serenity/AcceptanceTestSuite.java
+++ src/test/java/serenity/AcceptanceTestSuite.java
@@ -1,5 +1,26 @@
 package serenity;
 
 import net.serenitybdd.jbehave.SerenityStories;
+import org.junit.Before;
+import org.junit.Test;
 
-public class AcceptanceTestSuite extends SerenityStories {}
+import static org.junit.Assert.assertEquals;
+
+public class AcceptanceTestSuite extends SerenityStories {
+
+    private RedisBackedCache underTest;
+
+    @Before
+    public void setUp() {
+        // Assume that we have Redis running locally?
+        underTest = new RedisBackedCache("localhost", 6379);
+    }
+
+    @Test
+    public void testSimplePutAndGet() {
+        underTest.put("test", "example");
+
+        String retrieved = underTest.get("test");
+        assertEquals("example", retrieved);
+    }
+}
