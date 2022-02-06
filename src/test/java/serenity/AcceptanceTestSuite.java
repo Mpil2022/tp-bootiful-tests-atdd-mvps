02082e210fa5737dbaa6512208c0a5b76db33a6a
Omar <omar.bouayad26@gmail.com>
2022-02-06 21:40:49 +0100

    p

--- src/test/java/serenity/AcceptanceTestSuite.java
+++ src/test/java/serenity/AcceptanceTestSuite.java
@@ -1,26 +1,16 @@
 package serenity;
 
 import net.serenitybdd.jbehave.SerenityStories;
-import org.junit.Before;
-import org.junit.Test;
+import org.junit.Rule;
+import org.testcontainers.containers.GenericContainer;
+import org.testcontainers.utility.DockerImageName;
 
-import static org.junit.Assert.assertEquals;
 
 public class AcceptanceTestSuite extends SerenityStories {
 
-    private RedisBackedCache underTest;
-
-    @Before
-    public void setUp() {
-        // Assume that we have Redis running locally?
-        underTest = new RedisBackedCache("localhost", 6379);
-    }
-
-    @Test
-    public void testSimplePutAndGet() {
-        underTest.put("test", "example");
-
-        String retrieved = underTest.get("test");
-        assertEquals("example", retrieved);
-    }
+    // rule {
+    @Rule
+    public GenericContainer redis = new GenericContainer(DockerImageName.parse("redis:5.0.3-alpine"))
+            .withExposedPorts(8081);
+    // }
 }
